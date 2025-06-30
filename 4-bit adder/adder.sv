5fcca923f12e6822114584e5456371b820c10c0c
mariamtaher2000 <85410895+mariamtaher2000@users.noreply.github.com>
2025-06-30 22:17:14 +0300

    Update adder.sv

--- 4-bit adder/adder.sv
+++ 4-bit adder/adder.sv
@@ -1,1 +1,17 @@
+module adder(
+input      [3:0] a,b,
+output reg [4:0] y
+);
+
+always@(*)
+  begin
+    y = a + b;
+  end
+
+initial begin
+  $dumpfile("dump.vcd");
+  $dumpvars(1,adder);
+end
+
+endmodule
 
